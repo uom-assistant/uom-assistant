3bd70c8692496f76a24896f5a06dde2a78822b76
yrccondor <yrc371106@hotmail.com>
2021-03-06 03:04:54 +0800

    fix: wrong var name

--- backend/mail/action/index.php
+++ backend/mail/action/index.php
@@ -56,22 +56,22 @@ $data = get_post_data();
 if ($user['email'] === false || !isset($data['mail_id']) || !isset($data['action'])) {
     rest_die('Invalid request.', $conn);
 }
-if ($user['action'] !== 'junk' && $user['action'] !== 'flag' && $user['action'] !== 'detele' && $user['action'] !== 'unread' && $user['action'] !== 'allread') {
+if ($data['action'] !== 'junk' && $data['action'] !== 'flag' && $data['action'] !== 'detele' && $data['action'] !== 'unread' && $data['action'] !== 'allread') {
     rest_die('Invalid request.', $conn);
 }
 $id = (int)get_post_data()['mail_id'];
 
 $imap = imap_open('{outlook.office365.com:993/imap/ssl}INBOX', $user['email'], $user['password']);
 
-if ($user['action'] === 'junk') {
+if ($data['action'] === 'junk') {
     imap_mail_move($imap, strval(imap_msgno($imap, (int)$data['mail_id'])), '{outlook.office365.com:993/imap/ssl}Junk Email');
-} else if ($user['action'] === 'flag') {
+} else if ($data['action'] === 'flag') {
     imap_setflag_full($imap, strval(imap_msgno($imap, (int)$data['mail_id'])), '\\Flagged');
-} else if ($user['action'] === 'detele') {
+} else if ($data['action'] === 'detele') {
     imap_delete($imap, strval(imap_msgno($imap, (int)$data['mail_id'])));
-} else if ($user['action'] === 'unread') {
+} else if ($data['action'] === 'unread') {
     imap_clearflag_full($imap, strval(imap_msgno($imap, (int)$data['mail_id'])), '\\Seen');
-} else if ($user['action'] === 'allread') {
+} else if ($data['action'] === 'allread') {
     $all = imap_search($imap, 'UNDELETED UNSEEN ALL');
     imap_setflag_full($imap, implode(',', $all), '\\Seen');
 }
